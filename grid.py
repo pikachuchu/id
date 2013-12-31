d0cb1cd09942c8abfc2c50e6712401c3a1625ac3
Chuchu Ma <sarahma88@gmail.com>
2013-12-30 20:48:48 -0600

    reproduction multiple strength

--- grid.py
+++ grid.py
@@ -1,4 +1,5 @@
 import random
+import collections
 class Cell:
     # read only
     def __init__(self, team, strength):
@@ -36,7 +37,7 @@ class Grid:
         for row in range(self.length):
             for col in range(self.width * 2 / 5):
                 result = self.rand.random()
-                if result < .70:
+                if result < .75:
                     # <   1000 10000
                     # .25 .265
                     # .35 .385
@@ -48,24 +49,37 @@ class Grid:
                     # .70 .522 .5001
                     # .75 .488
                     self.cells[row][col] = Cell("R", 3)
-                    self.cells[row][self.width - col - 1] = Cell("R", 3) # TODO enmy
+                    self.cells[row][self.width - col - 1] = Cell("B", 3)
     def step(self):
         step = [[neutral() for col in range(self.width)] for row in range(self.length)]
         for row in range(self.length):
             for col in range(self.width):
-                friendly = 0
-                enemy = 0
+                counts = collections.Counter() 
+                strengths = collections.Counter()
                 for (r,c) in self.adj(row,col):
-                    if self.cells[r][c].team == self.cells[row][col].team:
-                        friendly += 1
-                    elif self.cells[r][c].team != neutralstr:
-                        enemy += 1
+                    counts[self.cells[r][c].team] += 1
+                    strengths[self.cells[r][c].team] += self.cells[r][c].strength
                 if self.cells[row][col].team != neutralstr:
+                    friendly = counts[self.cells[row][col].team]
                     if friendly >= 2 and friendly <= 3:
                         step[row][col] = self.cells[row][col]
                 else:
-                    if enemy == 3:
-                        step[row][col] = Cell("R", 3)
+                    threes = []
+                    for team, count in counts.items():
+                        if count == 3 and team != neutralstr:
+                            threes.append(team)
+                    if len(threes) == 1:
+                        strength = strengths[threes[0]] + self.rand.randint(1,9)
+                        step[row][col] = Cell(threes[0], strength / 4)
+                    if len(threes) == 2:
+                        if strengths[threes[0]] > strengths[threes[1]]:
+                            winner = threes[0]
+                        elif strengths[threes[0]] < strengths[threes[1]]:
+                            winner = threes[1]
+                        else:
+                            winner = threes[self.rand.randint(0,1)]
+                        strength = strengths[winner] + self.rand.randint(1,9)
+                        step[row][col] = Cell(winner, strength / 4)
         self.cells = step
         self.turn += 1
     def extinct(self):
@@ -77,7 +91,7 @@ class Grid:
 
 extinct = 0
 stable = 0
-for x in range(10000):
+for x in range(1000):
     grid = Grid()
     for y in range(40):
         grid.step()
